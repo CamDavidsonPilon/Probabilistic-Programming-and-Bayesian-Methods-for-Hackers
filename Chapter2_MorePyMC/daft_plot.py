c9d9a67026f2024ab87909c455f90dd7aadb4e18
Cam Davidson-Pilon <cam.davidson.pilon@gmail.com>
2013-04-20 10:40:36 -0400

    changes to Privacy Algo

--- Chapter2_MorePyMC/daft_plot.py
+++ Chapter2_MorePyMC/daft_plot.py
@@ -1,4 +1,7 @@
 #daft drawing for SMS example
+import matplotlib.pyplot as plt
+
+plt.figure( figsize=(12,5) )
 
 try:
     import daft
@@ -12,7 +15,7 @@ pgm.add_node(daft.Node("alpha", r"$\alpha$", 6, 4.0))
 pgm.add_node(daft.Node("lambda1", r"$\lambda_1$", 5.5, 3.2,))
 pgm.add_node(daft.Node("lambda2", r"$\lambda_2$", 6.5, 3.2))
 pgm.add_node(daft.Node("lambda", r"$\lambda$", 5.0, 2.0))
-pgm.add_node(daft.Node("obs", "SMS\nobs", 5.0, 1.0, 1.2, observed=True))
+pgm.add_node(daft.Node("obs", "obs", 5.0, 1.0, 1.2, observed=True))
 
 
 
