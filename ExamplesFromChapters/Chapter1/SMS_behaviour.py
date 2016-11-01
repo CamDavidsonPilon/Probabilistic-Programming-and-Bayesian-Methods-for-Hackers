05c705ba30e1ecf331b0efecf09931d03120ec8f
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2016-10-31 20:42:28 -0400

    Fix typos in code examples.

--- ExamplesFromChapters/Chapter1/SMS_behaviour.py
+++ ExamplesFromChapters/Chapter1/SMS_behaviour.py
@@ -7,7 +7,7 @@ n_count_data = len(count_data)
 alpha = 1.0 / count_data.mean()  # recall count_data is
                                  # the variable that holds our txt counts
 
-lambda_1 = pm.Exponential("lambda_1",  alpha)
+lambda_1 = pm.Exponential("lambda_1", alpha)
 lambda_2 = pm.Exponential("lambda_2", alpha)
 
 tau = pm.DiscreteUniform("tau", lower=0, upper=n_count_data)
