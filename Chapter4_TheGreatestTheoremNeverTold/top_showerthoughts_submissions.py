d14068bd23add68e4ea787e57b9de8e1a3f988b4
Kurisu Chan <miemiekurisu@aol.com>
2022-08-29 10:26:10 +0800

    Ch4 updated the  praw to 7.6.0 in top_showerthoughts_submissions  to make it work, and other small updates

--- Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
+++ Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
@@ -6,10 +6,16 @@ from IPython.core.display import Image
 import praw
 
 
-reddit = praw.Reddit("BayesianMethodsForHackers")
-subreddit  = reddit.get_subreddit("showerthoughts")
 
-top_submissions = subreddit.get_top(limit=100)
+#subreddit  = reddit.get_subreddit("showerthoughts")
+
+#top_submissions = subreddit.get_top(limit=100)
+
+#update old praw usage to current version (7.6.0)
+#please notice that new Reddit Object usage, especially the praw.ini file
+reddit = praw.Reddit("BayesianMethodsForHackers",user_agent="BMFH")
+top_submissions = reddit.subreddit("showerthoughts").new(limit=100)
+
 
 n_sub = int( sys.argv[1] ) if sys.argv[1] else 1
 
@@ -17,7 +23,6 @@ i = 0
 while i < n_sub:
     top_submission = next(top_submissions)
     i+=1
-
 top_post = top_submission.title
 
 upvotes = []
@@ -26,7 +31,7 @@ contents = []
 
 for sub in top_submissions:
     try:
-        ratio = reddit.get_submission(sub.permalink).upvote_ratio
+        ratio = sub.upvote_ratio
         ups = int(round((ratio*sub.score)/(2*ratio - 1)) if ratio != 0.5 else round(sub.score/2))
         upvotes.append(ups)
         downvotes.append(ups - sub.score)
